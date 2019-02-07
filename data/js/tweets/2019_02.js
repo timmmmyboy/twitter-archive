fb134b7ae4c9f00bdb68f596714a97587b6af7ed
Tim Owens <genial@gmail.com>
2019-02-06 20:19:48 -0500

    Added 1 tweets

--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -1,6 +1,232 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Wed Feb 06 15:07:14 +0000 2019",
+		"id": 1093164219615318000,
+		"id_str": "1093164219615318016",
+		"full_text": "Please Join VSTE board member @mswecker and VSTE Conference committee member @tamaraletter for a webinar as they discuss the importance of kindness. https://t.co/n9Mqtk4PwL  #vste #bekind #kindness https://t.co/cu2xfz9Rp1",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			197
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "vste",
+					"indices": [
+						174,
+						179
+					]
+				},
+				{
+					"text": "bekind",
+					"indices": [
+						180,
+						187
+					]
+				},
+				{
+					"text": "kindness",
+					"indices": [
+						188,
+						197
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "mswecker",
+					"name": "Meg Swecker",
+					"id": 8895172,
+					"id_str": "8895172",
+					"indices": [
+						30,
+						39
+					]
+				},
+				{
+					"screen_name": "tamaraletter",
+					"name": "Tamara Letter",
+					"id": 244707031,
+					"id_str": "244707031",
+					"indices": [
+						77,
+						90
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/n9Mqtk4PwL",
+					"expanded_url": "http://bit.ly/edwebkind",
+					"display_url": "bit.ly/edwebkind",
+					"indices": [
+						149,
+						172
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1093164207137267700,
+					"id_str": "1093164207137267712",
+					"indices": [
+						198,
+						221
+					],
+					"media_url": "http://pbs.twimg.com/media/DyuzDfeWoAAl9Io.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DyuzDfeWoAAl9Io.jpg",
+					"url": "https://t.co/cu2xfz9Rp1",
+					"display_url": "pic.twitter.com/cu2xfz9Rp1",
+					"expanded_url": "https://twitter.com/timmmmyboy/status/1093164219615318016/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1093164207137267700,
+					"id_str": "1093164207137267712",
+					"indices": [
+						198,
+						221
+					],
+					"media_url": "http://pbs.twimg.com/media/DyuzDfeWoAAl9Io.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DyuzDfeWoAAl9Io.jpg",
+					"url": "https://t.co/cu2xfz9Rp1",
+					"display_url": "pic.twitter.com/cu2xfz9Rp1",
+					"expanded_url": "https://twitter.com/timmmmyboy/status/1093164219615318016/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1025923491495903200,
+			"id_str": "1025923491495903232",
+			"name": "timmmmyboy",
+			"screen_name": "timmmmyboy",
+			"location": "Fredericksburg, VA",
+			"description": "Co-founder of Reclaim Hosting",
+			"url": "https://t.co/WRb8RPVfpf",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/WRb8RPVfpf",
+							"expanded_url": "https://blog.timowens.io",
+							"display_url": "blog.timowens.io",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 187,
+			"friends_count": 266,
+			"listed_count": 3,
+			"created_at": "Sun Aug 05 01:56:35 +0000 2018",
+			"favourites_count": 162,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 213,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1025923581417541633/YBNGcPYZ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1025923581417541633/YBNGcPYZ_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Please Join VSTE board member @mswecker and VSTE Conference committee member @tamaraletter for a webinar as they discuss the importance of kindness. https://t.co/n9Mqtk4PwL  #vste #bekind #kindness https://t.co/cu2xfz9Rp1"
+	},
+	{
 		"created_at": "Sat Feb 02 19:08:24 +0000 2019",
 		"id": 1091775360176402400,
 		"id_str": "1091775360176402435",
