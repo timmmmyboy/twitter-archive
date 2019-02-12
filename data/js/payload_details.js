2dc56eb21014cc2b5bed7fe0d6d2af1231e7bdfd
Tim Owens <genial@gmail.com>
2019-02-11 20:19:48 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 19646,
-	"created_at": "Sat Feb 09 01:19:47 +0000 2019"
+	"tweets": 19647,
+	"created_at": "Tue Feb 12 01:19:47 +0000 2019"
 }
--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -1,6 +1,237 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Tue Feb 12 00:56:10 +0000 2019",
+		"id": 1095124369783832600,
+		"id_str": "1095124369783832576",
+		"full_text": "Please Join VSTE Tech Coaches of the Year @Mrs_Demas @PHausEDU and @FCPS_Signorelli for a webinar on February 28th at 7:30pm. Join us at https://t.co/R6iMTbmUSS  To learn more about our winners, visit https://t.co/wGNPEF08MW  #vste https://t.co/gM2v0hPACR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			231
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "vste",
+					"indices": [
+						226,
+						231
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Mrs_Demas",
+					"name": "Krystle Demas",
+					"id": 1566452198,
+					"id_str": "1566452198",
+					"indices": [
+						42,
+						52
+					]
+				},
+				{
+					"screen_name": "PHausEDU",
+					"name": "Patrick Hausammann",
+					"id": 473036118,
+					"id_str": "473036118",
+					"indices": [
+						53,
+						62
+					]
+				},
+				{
+					"screen_name": "FCPS_Signorelli",
+					"name": "Timothy Signorelli",
+					"id": 730548003925168100,
+					"id_str": "730548003925168129",
+					"indices": [
+						67,
+						83
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/R6iMTbmUSS",
+					"expanded_url": "https://bit.ly/2N56VX2",
+					"display_url": "bit.ly/2N56VX2",
+					"indices": [
+						137,
+						160
+					]
+				},
+				{
+					"url": "https://t.co/wGNPEF08MW",
+					"expanded_url": "https://bit.ly/2MZd077",
+					"display_url": "bit.ly/2MZd077",
+					"indices": [
+						201,
+						224
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1095124365421826000,
+					"id_str": "1095124365421826049",
+					"indices": [
+						232,
+						255
+					],
+					"media_url": "http://pbs.twimg.com/media/DzKpzuHX0AEB7tO.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DzKpzuHX0AEB7tO.jpg",
+					"url": "https://t.co/gM2v0hPACR",
+					"display_url": "pic.twitter.com/gM2v0hPACR",
+					"expanded_url": "https://twitter.com/timmmmyboy/status/1095124369783832576/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1095124365421826000,
+					"id_str": "1095124365421826049",
+					"indices": [
+						232,
+						255
+					],
+					"media_url": "http://pbs.twimg.com/media/DzKpzuHX0AEB7tO.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DzKpzuHX0AEB7tO.jpg",
+					"url": "https://t.co/gM2v0hPACR",
+					"display_url": "pic.twitter.com/gM2v0hPACR",
+					"expanded_url": "https://twitter.com/timmmmyboy/status/1095124369783832576/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1025923491495903200,
+			"id_str": "1025923491495903232",
+			"name": "timmmmyboy",
+			"screen_name": "timmmmyboy",
+			"location": "Fredericksburg, VA",
+			"description": "Co-founder of Reclaim Hosting",
+			"url": "https://t.co/WRb8RPVfpf",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/WRb8RPVfpf",
+							"expanded_url": "https://blog.timowens.io",
+							"display_url": "blog.timowens.io",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 191,
+			"friends_count": 266,
+			"listed_count": 3,
+			"created_at": "Sun Aug 05 01:56:35 +0000 2018",
+			"favourites_count": 169,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 218,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1025923581417541633/YBNGcPYZ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1025923581417541633/YBNGcPYZ_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Please Join VSTE Tech Coaches of the Year @Mrs_Demas @PHausEDU and @FCPS_Signorelli for a webinar on February 28th at 7:30pm. Join us at https://t.co/R6iMTbmUSS  To learn more about our winners, visit https://t.co/wGNPEF08MW  #vste https://t.co/gM2v0hPACR"
+	},
+	{
 		"created_at": "Fri Feb 08 14:51:35 +0000 2019",
 		"id": 1093885055506747400,
 		"id_str": "1093885055506747395",
