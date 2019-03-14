5d4f1234efef87ff7359a91d37ac513a01be0ca5
Tim Owens <genial@gmail.com>
2019-03-13 21:19:48 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 19687,
-	"created_at": "Tue Mar 12 01:19:48 +0000 2019"
+	"tweets": 19688,
+	"created_at": "Thu Mar 14 01:19:48 +0000 2019"
 }
--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,161 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Wed Mar 13 22:56:39 +0000 2019",
+		"id": 1105965926489485300,
+		"id_str": "1105965926489485312",
+		"full_text": "@ROBODAVISWFISD @ChickfilA @Whataburger @WFISDschools @WFISD_CEC @SOLIDWORKSedu That’s awesome! Full disclosure, I didn’t design it, the FB post was just me sharing what I had found with a credit to designer. (I do love me some CFA Sauce though)",
+		"truncated": false,
+		"display_text_range": [
+			80,
+			245
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ROBODAVISWFISD",
+					"name": "Jeffrey Scott Davis M. Ed.",
+					"id": 791633281024340000,
+					"id_str": "791633281024339968",
+					"indices": [
+						0,
+						15
+					]
+				},
+				{
+					"screen_name": "ChickfilA",
+					"name": "Chick-fil-A, Inc.",
+					"id": 16043308,
+					"id_str": "16043308",
+					"indices": [
+						16,
+						26
+					]
+				},
+				{
+					"screen_name": "Whataburger",
+					"name": "Whataburger®",
+					"id": 353060311,
+					"id_str": "353060311",
+					"indices": [
+						27,
+						39
+					]
+				},
+				{
+					"screen_name": "WFISDschools",
+					"name": "Wichita Falls ISD",
+					"id": 2767436088,
+					"id_str": "2767436088",
+					"indices": [
+						40,
+						53
+					]
+				},
+				{
+					"screen_name": "WFISD_CEC",
+					"name": "WFISD CEC",
+					"id": 860213547858579500,
+					"id_str": "860213547858579462",
+					"indices": [
+						54,
+						64
+					]
+				},
+				{
+					"screen_name": "SOLIDWORKSedu",
+					"name": "SOLIDWORKS Education",
+					"id": 1661202283,
+					"id_str": "1661202283",
+					"indices": [
+						65,
+						79
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": 1105922286371176400,
+		"in_reply_to_status_id_str": "1105922286371176448",
+		"in_reply_to_user_id": 791633281024340000,
+		"in_reply_to_user_id_str": "791633281024339968",
+		"in_reply_to_screen_name": "ROBODAVISWFISD",
+		"user": {
+			"id": 1025923491495903200,
+			"id_str": "1025923491495903232",
+			"name": "timmmmyboy",
+			"screen_name": "timmmmyboy",
+			"location": "Fredericksburg, VA",
+			"description": "Co-founder of Reclaim Hosting",
+			"url": "https://t.co/WRb8RPVfpf",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/WRb8RPVfpf",
+							"expanded_url": "https://blog.timowens.io",
+							"display_url": "blog.timowens.io",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 195,
+			"friends_count": 267,
+			"listed_count": 3,
+			"created_at": "Sun Aug 05 01:56:35 +0000 2018",
+			"favourites_count": 188,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 269,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1025923581417541633/YBNGcPYZ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1025923581417541633/YBNGcPYZ_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@ROBODAVISWFISD @ChickfilA @Whataburger @WFISDschools @WFISD_CEC @SOLIDWORKSedu That’s awesome! Full disclosure, I didn’t design it, the FB post was just me sharing what I had found with a credit to designer. (I do love me some CFA Sauce though)"
+	},
+	{
 		"created_at": "Tue Mar 12 00:17:46 +0000 2019",
 		"id": 1105261562992967700,
 		"id_str": "1105261562992967681",
