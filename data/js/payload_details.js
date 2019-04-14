55b08c7e413388b42dd0f1d7251a1a3ea2d67208
Tim Owens <genial@gmail.com>
2019-04-13 21:19:48 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 19733,
-	"created_at": "Sat Apr 13 01:19:47 +0000 2019"
+	"tweets": 19734,
+	"created_at": "Sun Apr 14 01:19:47 +0000 2019"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,112 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Sat Apr 13 20:51:10 +0000 2019",
+		"id": 1117162118418059300,
+		"id_str": "1117168370540003328",
+		"full_text": "Now Playing: The Computer Chronicles - Bill Gates' Profile (1998) You can watch live at https://t.co/0waLCV9M4J",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			111
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/0waLCV9M4J",
+					"expanded_url": "http://reclaimvideo.com/live",
+					"display_url": "reclaimvideo.com/live",
+					"indices": [
+						88,
+						111
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://reclaimvideo.com\" rel=\"nofollow\">Reclaim Robot</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 885452192047648800,
+			"id_str": "885452192047648768",
+			"name": "Reclaim Video",
+			"screen_name": "reclaimvideo",
+			"location": "Fredericksburg, VA",
+			"description": "Reclaim Video is a cutting-edge video rental store in Fredericksburg, Virginia housing a wide selection of VHS, Laserdisc, Betamax and more.",
+			"url": "https://t.co/2BKCC77oHs",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/2BKCC77oHs",
+							"expanded_url": "http://reclaimvideo.com",
+							"display_url": "reclaimvideo.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 83,
+			"friends_count": 48,
+			"listed_count": 2,
+			"created_at": "Thu Jul 13 10:53:48 +0000 2017",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 45,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/960656952094113792/f52C3HYO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/960656952094113792/f52C3HYO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/885452192047648768/1517873418",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Now Playing: The Computer Chronicles - Bill Gates' Profile (1998) You can watch live at https://t.co/0waLCV9M4J"
+	},
+	{
 		"created_at": "Fri Apr 12 21:23:59 +0000 2019",
 		"id": 1116814241527418900,
 		"id_str": "1116814241527418882",
