fe6c9ba0c432e0ac457fd9e8676bb910ea45f427
Tim Owens <genial@gmail.com>
2019-10-01 21:19:49 -0400

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 19943,
-	"created_at": "Tue Oct 01 01:19:48 +0000 2019"
+	"tweets": 19945,
+	"created_at": "Wed Oct 02 01:19:48 +0000 2019"
 }
--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,6 +1,426 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Tue Oct 01 20:26:54 +0000 2019",
+		"id": 1179130588625854500,
+		"id_str": "1179130588625854466",
+		"full_text": "Also we’re hiring! Join this fine group of people https://t.co/kWB6GWJaeW https://t.co/MW3OfYbesi",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			97
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/kWB6GWJaeW",
+					"expanded_url": "https://reclaimhosting.com/careers/customer-support-specialist/",
+					"display_url": "reclaimhosting.com/careers/custom…",
+					"indices": [
+						50,
+						73
+					]
+				},
+				{
+					"url": "https://t.co/MW3OfYbesi",
+					"expanded_url": "https://twitter.com/meredithfierro/status/1179122734636376065",
+					"display_url": "twitter.com/meredithfierro…",
+					"indices": [
+						74,
+						97
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1025923491495903200,
+			"id_str": "1025923491495903232",
+			"name": "timmmmyboy",
+			"screen_name": "timmmmyboy",
+			"location": "Fredericksburg, VA",
+			"description": "Co-founder of Reclaim Hosting",
+			"url": "https://t.co/WRb8RPVfpf",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/WRb8RPVfpf",
+							"expanded_url": "https://blog.timowens.io",
+							"display_url": "blog.timowens.io",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 265,
+			"friends_count": 282,
+			"listed_count": 8,
+			"created_at": "Sun Aug 05 01:56:35 +0000 2018",
+			"favourites_count": 381,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 621,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1025923581417541633/YBNGcPYZ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1025923581417541633/YBNGcPYZ_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1179122734636376000,
+		"quoted_status_id_str": "1179122734636376065",
+		"quoted_status_permalink": {
+			"url": "https://t.co/MW3OfYbesi",
+			"expanded": "https://twitter.com/meredithfierro/status/1179122734636376065",
+			"display": "twitter.com/meredithfierro…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Oct 01 19:55:41 +0000 2019",
+			"id": 1179122734636376000,
+			"id_str": "1179122734636376065",
+			"full_text": "This doesn't happen too often with time zones and different shift times, but today, we've got all @ReclaimHosting Reclaimer's under one Slack roof 🏠 https://t.co/ZmMwYyn3sR",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				148
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "ReclaimHosting",
+						"name": "Reclaim Hosting",
+						"id": 1602053274,
+						"id_str": "1602053274",
+						"indices": [
+							98,
+							113
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 1179122731314503700,
+						"id_str": "1179122731314503681",
+						"indices": [
+							149,
+							172
+						],
+						"media_url": "http://pbs.twimg.com/media/EF0V4HqW4AERM5t.png",
+						"media_url_https": "https://pbs.twimg.com/media/EF0V4HqW4AERM5t.png",
+						"url": "https://t.co/ZmMwYyn3sR",
+						"display_url": "pic.twitter.com/ZmMwYyn3sR",
+						"expanded_url": "https://twitter.com/meredithfierro/status/1179122734636376065/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 228,
+								"h": 254,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 228,
+								"h": 254,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 228,
+								"h": 254,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1179122731314503700,
+						"id_str": "1179122731314503681",
+						"indices": [
+							149,
+							172
+						],
+						"media_url": "http://pbs.twimg.com/media/EF0V4HqW4AERM5t.png",
+						"media_url_https": "https://pbs.twimg.com/media/EF0V4HqW4AERM5t.png",
+						"url": "https://t.co/ZmMwYyn3sR",
+						"display_url": "pic.twitter.com/ZmMwYyn3sR",
+						"expanded_url": "https://twitter.com/meredithfierro/status/1179122734636376065/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 228,
+								"h": 254,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 228,
+								"h": 254,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 228,
+								"h": 254,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 113149236,
+				"id_str": "113149236",
+				"name": "Meredith Fierro",
+				"screen_name": "meredithfierro",
+				"location": "Fredericksburg, VA",
+				"description": "Customer Support Specialist for @ReclaimHosting | @CoWorkFXBG | @ReclaimVideo",
+				"url": "https://t.co/VYc50EQl0k",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/VYc50EQl0k",
+								"expanded_url": "http://meredithfierro.com",
+								"display_url": "meredithfierro.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 267,
+				"friends_count": 279,
+				"listed_count": 10,
+				"created_at": "Wed Feb 10 21:36:10 +0000 2010",
+				"favourites_count": 2795,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 2110,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/862075351601750016/Dqx8xGMV_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/862075351601750016/Dqx8xGMV_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/113149236/1562186177",
+				"profile_link_color": "1B95E0",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 6,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 1,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Also we’re hiring! Join this fine group of people https://t.co/kWB6GWJaeW https://t.co/MW3OfYbesi"
+	},
+	{
+		"created_at": "Tue Oct 01 17:12:37 +0000 2019",
+		"id": 1179081698845319200,
+		"id_str": "1179081698845319171",
+		"full_text": "@mburtis I have no doubt @OnlineCrsLady has stuff she could link you up to.",
+		"truncated": false,
+		"display_text_range": [
+			9,
+			75
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "mburtis",
+					"name": "martha",
+					"id": 744533,
+					"id_str": "744533",
+					"indices": [
+						0,
+						8
+					]
+				},
+				{
+					"screen_name": "OnlineCrsLady",
+					"name": "Laura Gibbs",
+					"id": 7044082,
+					"id_str": "7044082",
+					"indices": [
+						25,
+						39
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1179071531462226000,
+		"in_reply_to_status_id_str": "1179071531462225920",
+		"in_reply_to_user_id": 744533,
+		"in_reply_to_user_id_str": "744533",
+		"in_reply_to_screen_name": "mburtis",
+		"user": {
+			"id": 1025923491495903200,
+			"id_str": "1025923491495903232",
+			"name": "timmmmyboy",
+			"screen_name": "timmmmyboy",
+			"location": "Fredericksburg, VA",
+			"description": "Co-founder of Reclaim Hosting",
+			"url": "https://t.co/WRb8RPVfpf",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/WRb8RPVfpf",
+							"expanded_url": "https://blog.timowens.io",
+							"display_url": "blog.timowens.io",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 265,
+			"friends_count": 282,
+			"listed_count": 8,
+			"created_at": "Sun Aug 05 01:56:35 +0000 2018",
+			"favourites_count": 381,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 621,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1025923581417541633/YBNGcPYZ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1025923581417541633/YBNGcPYZ_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@mburtis I have no doubt @OnlineCrsLady has stuff she could link you up to."
+	},
+	{
 		"created_at": "Tue Oct 01 00:34:31 +0000 2019",
 		"id": 1178830518818922500,
 		"id_str": "1178830518818922499",
