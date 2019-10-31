586a1071434afd10cf51a669e2d13e7433f4eca4
Tim Owens <genial@gmail.com>
2019-10-30 21:19:48 -0400

    Added 1 tweets

--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,6 +1,124 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Wed Oct 30 21:52:36 +0000 2019",
+		"id": 1189661403126345700,
+		"id_str": "1189661403126345728",
+		"full_text": "Today we went live with our plans for @ReclaimArcade and it feels so good. If you’re a FB user and want to follow along check it all out https://t.co/gcIsz7hP7A",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			160
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ReclaimArcade",
+					"name": "Reclaim Arcade",
+					"id": 1174481114192384000,
+					"id_str": "1174481114192384000",
+					"indices": [
+						38,
+						52
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/gcIsz7hP7A",
+					"expanded_url": "https://www.facebook.com/reclaimarcade/videos/422814048647273/?__xts__[0]=68.ARDYeVhQIWXc-bYKYzzOPV2BjmvtRiVZmjvo55828CcuhdVHNdDTYhdBedS7jGhTbRJAuGlAGL_Vg7Vd92sr-PtR8hALFAYFsD_5Q8EvWvS3WJKRy9JwU74HnIGlczwwHtdbUKOcUlcHL6lDWmEZEEMk74WW9mMblgT8P6nZOp1iNmRPsxv7Ey77M4hRUvjeU1xJ22pRBEUP4cnlQn19z1bHVzV3TClKGcLZzB39YVD9XHAcE1Y67FlK5cn5vWohhdYcwW_pgHIrDmPVkSNXBas1MRR9-ic6UMPUjzo-JOa6YF8WYyiRp_lh5bUAHUs3NYfvGgk7jCSqRxilHSQbUvt8Lc68kdt8C3s&__tn__=-R",
+					"display_url": "facebook.com/reclaimarcade/…",
+					"indices": [
+						137,
+						160
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1025923491495903200,
+			"id_str": "1025923491495903232",
+			"name": "timmmmyboy",
+			"screen_name": "timmmmyboy",
+			"location": "Fredericksburg, VA",
+			"description": "Co-founder of Reclaim Hosting",
+			"url": "https://t.co/WRb8RPVfpf",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/WRb8RPVfpf",
+							"expanded_url": "https://blog.timowens.io",
+							"display_url": "blog.timowens.io",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 268,
+			"friends_count": 288,
+			"listed_count": 7,
+			"created_at": "Sun Aug 05 01:56:35 +0000 2018",
+			"favourites_count": 403,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 664,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1025923581417541633/YBNGcPYZ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1025923581417541633/YBNGcPYZ_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Today we went live with our plans for @ReclaimArcade and it feels so good. If you’re a FB user and want to follow along check it all out https://t.co/gcIsz7hP7A"
+	},
+	{
 		"created_at": "Mon Oct 28 11:53:10 +0000 2019",
 		"id": 1188785778454868000,
 		"id_str": "1188785778454867968",
