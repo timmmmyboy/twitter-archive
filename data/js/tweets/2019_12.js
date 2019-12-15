6f3591aac33290eb20d6e1a6c636315f742d3b3b
Tim Owens <genial@gmail.com>
2019-12-14 20:19:48 -0500

    Added 1 tweets

--- data/js/tweets/2019_12.js
+++ data/js/tweets/2019_12.js
@@ -1,10 +1,117 @@
 Grailbird.data.tweets_2019_12 = 
 [
 	{
+		"created_at": "Sun Dec 15 00:48:08 +0000 2019",
+		"id": 1206013033727905800,
+		"id_str": "1206013033727905792",
+		"full_text": "@DrGarcia Delightfully weird",
+		"truncated": false,
+		"display_text_range": [
+			10,
+			28
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "DrGarcia",
+					"name": "GNA Garcia",
+					"id": 19897497,
+					"id_str": "19897497",
+					"indices": [
+						0,
+						9
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1205962830165332000,
+		"in_reply_to_status_id_str": "1205962830165331968",
+		"in_reply_to_user_id": 19897497,
+		"in_reply_to_user_id_str": "19897497",
+		"in_reply_to_screen_name": "DrGarcia",
+		"user": {
+			"id": 1025923491495903200,
+			"id_str": "1025923491495903232",
+			"name": "timmmmyboy",
+			"screen_name": "timmmmyboy",
+			"location": "Fredericksburg, VA",
+			"description": "Co-founder of Reclaim Hosting and Reclaim Arcade",
+			"url": "https://t.co/WRb8RPVfpf",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/WRb8RPVfpf",
+							"expanded_url": "https://blog.timowens.io",
+							"display_url": "blog.timowens.io",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 273,
+			"friends_count": 288,
+			"listed_count": 6,
+			"created_at": "Sun Aug 05 01:56:35 +0000 2018",
+			"favourites_count": 450,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 720,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1202343501767331840/OzYWoDA__normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1202343501767331840/OzYWoDA__normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@DrGarcia Delightfully weird"
+	},
+	{
 		"created_at": "Wed Dec 11 03:37:24 +0000 2019",
 		"id": 1204606079499427800,
 		"id_str": "1204606079499427840",
-		"full_text": "@witchyrichy @KatieF @MrPStrunk @ScottLewisITC @vste @tamaraletter @ERobbPrincipal @champforkids @rodcarnill @dhorndorff @vtpanda @chrlafferty @PHausEDU @misskoplin Ah I think I have to clip out the bg music from before the session began, lol. Should be up tomorrow 🤦🏻‍♂️",
+		"full_text": "@witchyrichy @KatieF @MrPStrunk @ScottLewisITC @vste @tamaraletter @ERobbPrincipal @champforkids @rodcarnill @dhorndorff @vtpanda @chrlafferty @PHausEDU @misskoplin Ah I think I have to clip out the bg music from before the session began, lol. Should be up tomorrow 🤦🏻♂️",
 		"truncated": false,
 		"display_text_range": [
 			165,
@@ -235,7 +342,7 @@ Grailbird.data.tweets_2019_12 =
 		"favorited": false,
 		"retweeted": false,
 		"lang": "en",
-		"text": "@witchyrichy @KatieF @MrPStrunk @ScottLewisITC @vste @tamaraletter @ERobbPrincipal @champforkids @rodcarnill @dhorndorff @vtpanda @chrlafferty @PHausEDU @misskoplin Ah I think I have to clip out the bg music from before the session began, lol. Should be up tomorrow 🤦🏻‍♂️"
+		"text": "@witchyrichy @KatieF @MrPStrunk @ScottLewisITC @vste @tamaraletter @ERobbPrincipal @champforkids @rodcarnill @dhorndorff @vtpanda @chrlafferty @PHausEDU @misskoplin Ah I think I have to clip out the bg music from before the session began, lol. Should be up tomorrow 🤦🏻♂️"
 	},
 	{
 		"created_at": "Sun Dec 08 12:12:00 +0000 2019",
