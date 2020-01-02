09b3f2a92abf6e48fe2ff43e48d973330aeecfcf
Tim Owens <genial@gmail.com>
2020-01-01 20:19:49 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 20023,
-	"created_at": "Wed Jan 01 01:19:47 +0000 2020"
+	"tweets": 20024,
+	"created_at": "Thu Jan 02 01:19:48 +0000 2020"
 }
---
+++ data/js/tweets/2020_01.js
@@ -0,0 +1,232 @@
+Grailbird.data.tweets_2020_01 = 
+[
+	{
+		"created_at": "Wed Jan 01 21:35:47 +0000 2020",
+		"id": 1212487607696941000,
+		"id_str": "1212487607696941060",
+		"full_text": "It's getting harder and harder to leave the office now https://t.co/5IFsOnhjol",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			78
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/5IFsOnhjol",
+					"expanded_url": "https://twitter.com/ReclaimArcade/status/1212487305690259461",
+					"display_url": "twitter.com/ReclaimArcade/…",
+					"indices": [
+						55,
+						78
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1025923491495903200,
+			"id_str": "1025923491495903232",
+			"name": "timmmmyboy",
+			"screen_name": "timmmmyboy",
+			"location": "Fredericksburg, VA",
+			"description": "Co-founder of Reclaim Hosting and Reclaim Arcade",
+			"url": "https://t.co/WRb8RPVfpf",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/WRb8RPVfpf",
+							"expanded_url": "https://blog.timowens.io",
+							"display_url": "blog.timowens.io",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 276,
+			"friends_count": 287,
+			"listed_count": 6,
+			"created_at": "Sun Aug 05 01:56:35 +0000 2018",
+			"favourites_count": 453,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 724,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1202343501767331840/OzYWoDA__normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1202343501767331840/OzYWoDA__normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1212487305690259500,
+		"quoted_status_id_str": "1212487305690259461",
+		"quoted_status_permalink": {
+			"url": "https://t.co/5IFsOnhjol",
+			"expanded": "https://twitter.com/ReclaimArcade/status/1212487305690259461",
+			"display": "twitter.com/ReclaimArcade/…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Jan 01 21:34:35 +0000 2020",
+			"id": 1212487305690259500,
+			"id_str": "1212487305690259461",
+			"full_text": "New pin for the lineup. We couldn't wait until late Spring to grab this one and we'll be bringing it to the #frostybrewthru on January 25th, so grab a ticket and come on out! https://t.co/dmagpBS3hr",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				198
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "frostybrewthru",
+						"indices": [
+							108,
+							123
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/dmagpBS3hr",
+						"expanded_url": "https://www.instagram.com/p/B6ytP_mgPKn/?igshid=iqj24my97qcj",
+						"display_url": "instagram.com/p/B6ytP_mgPKn/…",
+						"indices": [
+							175,
+							198
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://instagram.com\" rel=\"nofollow\">Instagram</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1174481114192384000,
+				"id_str": "1174481114192384000",
+				"name": "Reclaim Arcade",
+				"screen_name": "ReclaimArcade",
+				"location": "Fredericksburg, Virginia",
+				"description": "Reclaim Arcade is a unique retro-modern entertainment venue merging popular vintage arcade games and pinball of the 70s, 80s, and 90s with a modern atmosphere.",
+				"url": "https://t.co/FsOdkzvbyk",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/FsOdkzvbyk",
+								"expanded_url": "https://reclaimarcade.com",
+								"display_url": "reclaimarcade.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 61,
+				"friends_count": 0,
+				"listed_count": 0,
+				"created_at": "Thu Sep 19 00:31:45 +0000 2019",
+				"favourites_count": 17,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 22,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1189579448678584321/fVH5bAI1_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1189579448678584321/fVH5bAI1_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1174481114192384000/1572452817",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"can_media_tag": true,
+				"followed_by": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 0,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "It's getting harder and harder to leave the office now https://t.co/5IFsOnhjol"
+	}
+]
