d08e6d04e6f4b5aa52f14d24fe26c4a0fbedb000
Tim Owens <genial@gmail.com>
2020-07-04 21:19:49 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 20588,
-	"created_at": "Sat Jul 04 01:19:47 +0000 2020"
+	"tweets": 20589,
+	"created_at": "Sun Jul 05 01:19:47 +0000 2020"
 }
--- data/js/tweets/2020_07.js
+++ data/js/tweets/2020_07.js
@@ -1,6 +1,139 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Sat Jul 04 17:11:10 +0000 2020",
+		"id": 1279462769901871000,
+		"id_str": "1279462769901871105",
+		"full_text": "Hey #DoOO peeps, proposal for a monthly meetup in the style of the Homebrew Club by @ChrisAldrich. Thinking it could be a lot of fun to do something like this. If it peaks your interest hit up the thread at https://t.co/ybZMxI960v or post at https://t.co/X24RYWSbQM.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			266
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "DoOO",
+					"indices": [
+						4,
+						9
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ChrisAldrich",
+					"name": "Chris Aldrich",
+					"id": 13645402,
+					"id_str": "13645402",
+					"indices": [
+						84,
+						97
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/ybZMxI960v",
+					"expanded_url": "https://community.reclaimhosting.com/t/recurring-domain-of-ones-own-online-meetup/3079",
+					"display_url": "community.reclaimhosting.com/t/recurring-do…",
+					"indices": [
+						207,
+						230
+					]
+				},
+				{
+					"url": "https://t.co/X24RYWSbQM",
+					"expanded_url": "https://boffosocko.com/2020/07/03/55773093/",
+					"display_url": "boffosocko.com/2020/07/03/557…",
+					"indices": [
+						242,
+						265
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1025923491495903200,
+			"id_str": "1025923491495903232",
+			"name": "timmmmyboy",
+			"screen_name": "timmmmyboy",
+			"location": "Fredericksburg, VA",
+			"description": "Co-founder of Reclaim Hosting and Reclaim Arcade",
+			"url": "https://t.co/WRb8RPVfpf",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/WRb8RPVfpf",
+							"expanded_url": "https://blog.timowens.io",
+							"display_url": "blog.timowens.io",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 339,
+			"friends_count": 288,
+			"listed_count": 6,
+			"created_at": "Sun Aug 05 01:56:35 +0000 2018",
+			"favourites_count": 833,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1367,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1202343501767331840/OzYWoDA__normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1202343501767331840/OzYWoDA__normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Hey #DoOO peeps, proposal for a monthly meetup in the style of the Homebrew Club by @ChrisAldrich. Thinking it could be a lot of fun to do something like this. If it peaks your interest hit up the thread at https://t.co/ybZMxI960v or post at https://t.co/X24RYWSbQM."
+	},
+	{
 		"created_at": "Fri Jul 03 21:27:40 +0000 2020",
 		"id": 1279139568961241000,
 		"id_str": "1279164934614593549",
