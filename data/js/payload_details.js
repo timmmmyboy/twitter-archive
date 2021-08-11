358fa6269b65d04d2e2f09f5ba8ad2f09447c7ae
Tim Owens <genial@gmail.com>
2021-08-10 21:19:51 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 20843,
-	"created_at": "Sun Jul 25 01:19:47 +0000 2021"
+	"tweets": 20844,
+	"created_at": "Wed Aug 11 01:19:48 +0000 2021"
 }
---
+++ data/js/tweets/2021_08.js
@@ -0,0 +1,352 @@
+Grailbird.data.tweets_2021_08 = 
+[
+	{
+		"created_at": "Tue Aug 10 04:07:09 +0000 2021",
+		"id": 1424905390895022000,
+		"id_str": "1424945387840983042",
+		"full_text": "You mean like sharing a pic of you and your chefstie??!! Oh, yes, I do. #cookingWfriends @timmmmyboy ((don't block me)) 🖤🖤🖤 hashtag dumplings https://t.co/52aurcltwP https://t.co/o0M4PPZeh6",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			165
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "cookingWfriends",
+					"indices": [
+						72,
+						88
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "timmmmyboy",
+					"name": "timmmmyboy",
+					"id": 1025923491495903200,
+					"id_str": "1025923491495903232",
+					"indices": [
+						89,
+						100
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/52aurcltwP",
+					"expanded_url": "https://twitter.com/timmmmyboy/status/1424904333997625344",
+					"display_url": "twitter.com/timmmmyboy/sta…",
+					"indices": [
+						142,
+						165
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1424905388072349700,
+					"id_str": "1424905388072349697",
+					"indices": [
+						166,
+						189
+					],
+					"media_url": "http://pbs.twimg.com/media/E8ZH5qaXsAEyR13.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E8ZH5qaXsAEyR13.jpg",
+					"url": "https://t.co/o0M4PPZeh6",
+					"display_url": "pic.twitter.com/o0M4PPZeh6",
+					"expanded_url": "https://twitter.com/DrGarcia/status/1424905390895022080/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1424905388072349700,
+					"id_str": "1424905388072349697",
+					"indices": [
+						166,
+						189
+					],
+					"media_url": "http://pbs.twimg.com/media/E8ZH5qaXsAEyR13.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E8ZH5qaXsAEyR13.jpg",
+					"url": "https://t.co/o0M4PPZeh6",
+					"display_url": "pic.twitter.com/o0M4PPZeh6",
+					"expanded_url": "https://twitter.com/DrGarcia/status/1424905390895022080/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 19897497,
+			"id_str": "19897497",
+			"name": "GNA Garcia",
+			"screen_name": "DrGarcia",
+			"location": "Provolt, OR",
+			"description": "Looking backwards and forwards, and still wondering.",
+			"url": "https://t.co/NggqAPdnl8",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/NggqAPdnl8",
+							"expanded_url": "http://www.gnagarcia.wordpress.com",
+							"display_url": "gnagarcia.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1159,
+			"friends_count": 316,
+			"listed_count": 107,
+			"created_at": "Mon Feb 02 07:42:51 +0000 2009",
+			"favourites_count": 9877,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 84381,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "BADFCD",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme12/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme12/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1414363349047910404/JPgIEhKH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1414363349047910404/JPgIEhKH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/19897497/1622234752",
+			"profile_link_color": "FF0000",
+			"profile_sidebar_border_color": "F2E195",
+			"profile_sidebar_fill_color": "FFF7CC",
+			"profile_text_color": "0C3E53",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "4ee7712e521a7387",
+			"url": "https://api.twitter.com/1.1/geo/id/4ee7712e521a7387.json",
+			"place_type": "city",
+			"name": "Fredericksburg",
+			"full_name": "Fredericksburg, VA",
+			"country_code": "US",
+			"country": "United States",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-77.5193868,
+							38.270129
+						],
+						[
+							-77.447189,
+							38.270129
+						],
+						[
+							-77.447189,
+							38.3260007
+						],
+						[
+							-77.5193868,
+							38.3260007
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1424904333997625300,
+		"quoted_status_id_str": "1424904333997625344",
+		"quoted_status_permalink": {
+			"url": "https://t.co/52aurcltwP",
+			"expanded": "https://twitter.com/timmmmyboy/status/1424904333997625344",
+			"display": "twitter.com/timmmmyboy/sta…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Aug 10 01:24:01 +0000 2021",
+			"id": 1424904333997625300,
+			"id_str": "1424904333997625344",
+			"full_text": "Discussing Twitter best practices with @DrGarcia and like where this is going. Maybe I could still use place for something…",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				123
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "DrGarcia",
+						"name": "GNA Garcia",
+						"id": 19897497,
+						"id_str": "19897497",
+						"indices": [
+							39,
+							48
+						]
+					}
+				],
+				"urls": []
+			},
+			"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1025923491495903200,
+				"id_str": "1025923491495903232",
+				"name": "timmmmyboy",
+				"screen_name": "timmmmyboy",
+				"location": "Fredericksburg, VA",
+				"description": "Co-founder of Reclaim Hosting and Reclaim Arcade",
+				"url": "https://t.co/WRb8RPVfpf",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/WRb8RPVfpf",
+								"expanded_url": "https://blog.timowens.io",
+								"display_url": "blog.timowens.io",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": true,
+				"followers_count": 389,
+				"friends_count": 133,
+				"listed_count": 5,
+				"created_at": "Sun Aug 05 01:56:35 +0000 2018",
+				"favourites_count": 1003,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 1692,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1202343501767331840/OzYWoDA__normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1202343501767331840/OzYWoDA__normal.jpg",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 0,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 1,
+		"favorite_count": 2,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "You mean like sharing a pic of you and your chefstie??!! Oh, yes, I do. #cookingWfriends @timmmmyboy ((don't block me)) 🖤🖤🖤 hashtag dumplings https://t.co/52aurcltwP https://t.co/o0M4PPZeh6"
+	}
+]
