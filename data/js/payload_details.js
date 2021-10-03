b85f22b6e7916eff227bd3713b2f79fcb48ec0af
Tim Owens <genial@gmail.com>
2021-10-02 21:19:47 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 20860,
-	"created_at": "Wed Sep 29 01:19:47 +0000 2021"
+	"tweets": 20861,
+	"created_at": "Sun Oct 03 01:19:47 +0000 2021"
 }
---
+++ data/js/tweets/2021_10.js
@@ -0,0 +1,99 @@
+Grailbird.data.tweets_2021_10 = 
+[
+	{
+		"created_at": "Sat Oct 02 02:15:25 +0000 2021",
+		"id": 1444038511854866400,
+		"id_str": "1444123825835229188",
+		"full_text": "We try to be really thoughtful about the incentives and rewards that encourage participation in the Glitch community, and there are no behaviors we want to encourage more than thoughtful creation and curation. And no reward more fitting than a new domain where your work can live.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			280
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1444038510642667500,
+		"in_reply_to_status_id_str": "1444038510642667523",
+		"in_reply_to_user_id": 36823,
+		"in_reply_to_user_id_str": "36823",
+		"in_reply_to_screen_name": "anildash",
+		"user": {
+			"id": 36823,
+			"id_str": "36823",
+			"name": "anildash",
+			"screen_name": "anildash",
+			"location": "NYC",
+			"description": "helping make @glitch • big fan of being a big fan of things • he/him",
+			"url": "https://t.co/iRPuzZYjgB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/iRPuzZYjgB",
+							"expanded_url": "https://anildash.com/about/",
+							"display_url": "anildash.com/about/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 579794,
+			"friends_count": 1070,
+			"listed_count": 8600,
+			"created_at": "Sat Dec 02 09:15:15 +0000 2006",
+			"favourites_count": 776692,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 234324,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1436967784274092032/BxvksM62_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1436967784274092032/BxvksM62_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/36823/1530683410",
+			"profile_link_color": "800080",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 9,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "We try to be really thoughtful about the incentives and rewards that encourage participation in the Glitch community, and there are no behaviors we want to encourage more than thoughtful creation and curation. And no reward more fitting than a new domain where your work can live."
+	}
+]
