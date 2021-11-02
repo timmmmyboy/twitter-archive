f580b359b129dfa1475aaccaae0a5f8a7a79779d
Tim Owens <genial@gmail.com>
2021-11-01 21:19:47 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 20867,
-	"created_at": "Sat Oct 30 01:19:47 +0000 2021"
+	"tweets": 20868,
+	"created_at": "Tue Nov 02 01:19:47 +0000 2021"
 }
---
+++ data/js/tweets/2021_11.js
@@ -0,0 +1,136 @@
+Grailbird.data.tweets_2021_11 = 
+[
+	{
+		"created_at": "Mon Nov 01 17:19:24 +0000 2021",
+		"id": 1455008428221415400,
+		"id_str": "1455222956846682113",
+		"full_text": "We did an arcade-theme this year. Joust slayed the day. @reclaimarcade just keeps getting better #goodtimes #Halloween traditions 🖤🖤🖤👯‍♀️🎃\nhttps://t.co/tUCZbKVBIW",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			162
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "goodtimes",
+					"indices": [
+						97,
+						107
+					]
+				},
+				{
+					"text": "Halloween",
+					"indices": [
+						108,
+						118
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ReclaimArcade",
+					"name": "Reclaim Arcade",
+					"id": 1174481114192384000,
+					"id_str": "1174481114192384000",
+					"indices": [
+						56,
+						70
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/tUCZbKVBIW",
+					"expanded_url": "https://www.instagram.com/p/CVt2uSbNS8B/?utm_medium=share_sheet",
+					"display_url": "instagram.com/p/CVt2uSbNS8B/…",
+					"indices": [
+						139,
+						162
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 19897497,
+			"id_str": "19897497",
+			"name": "GNA Garcia",
+			"screen_name": "DrGarcia",
+			"location": "Remote ",
+			"description": "Looking backwards and forwards, and still wondering.",
+			"url": "https://t.co/NggqAPdnl8",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/NggqAPdnl8",
+							"expanded_url": "http://www.gnagarcia.wordpress.com",
+							"display_url": "gnagarcia.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1214,
+			"friends_count": 301,
+			"listed_count": 107,
+			"created_at": "Mon Feb 02 07:42:51 +0000 2009",
+			"favourites_count": 10034,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 84893,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "BADFCD",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme12/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme12/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1414363349047910404/JPgIEhKH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1414363349047910404/JPgIEhKH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/19897497/1622234752",
+			"profile_link_color": "FF0000",
+			"profile_sidebar_border_color": "F2E195",
+			"profile_sidebar_fill_color": "FFF7CC",
+			"profile_text_color": "0C3E53",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "We did an arcade-theme this year. Joust slayed the day. @reclaimarcade just keeps getting better #goodtimes #Halloween traditions 🖤🖤🖤👯‍♀️🎃\nhttps://t.co/tUCZbKVBIW"
+	}
+]
